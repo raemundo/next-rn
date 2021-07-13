40e8f7ab3482c73a88428f598a2fc992e7d51083
raemundo <amaiplusplus@gmail.com>
2021-07-13 02:28:58 +0200

    refactor: implement my conventions

--- examples/next-nav/App.tsx
+++ examples/next-nav/App.tsx
@@ -1,49 +1,8 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Link from "next-rn/link";
-import Head from "next-rn/head";
-import Image from "next-rn/image";
-import Script from "next-rn/script";
-import useRouting from "next-rn/router/use-routing";
-// See the pages/ folder for the next.js routes
-// everything else is confined in this file :)
-
-export function Home() {
-
-  return (
-    <View style={styles.container}>
-      <Head metaInfo={{ title: "my homepage" }} />
-      <Script />
-      <Image
-        style={{
-          width: 50,
-          height: 50,
-        }}
-        source={{
-          uri: "https://reactnative.dev/img/tiny_logo.png",
-        }}
-      />
-      <Text style={styles.text}>Home Screen 🥳</Text>
-      <Link style={{ color: 'green', fontSize: 20 }} routeName="profile">
-        Click me to open profile :)
-      </Link> 
-    </View>
-  );
-}
-
-export function Profile() {
-  const { goBack } = useRouting()
-
-  return (
-    <View style={styles.container}>
-      <Head metaInfo={{ title: "my profile" }} />
-      <Text style={styles.text}>Profile! 🏋️‍♀️</Text>
-      <Button text="👈 Go back" onPress={() => goBack()} />
-    </View>
-  );
-}
+import Home from "./pages/index";
+import Profile from "./pages/Profile";
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -54,44 +13,5 @@ export default function App() {
         <Stack.Screen name="profile" component={Profile} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
-}
-
-/**
- *
- *
- *
- *
- *
- *
- * styles, button, etc
- */
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 20,
-    margin: 20,
-  },
-});
-
-function Button({ text, onPress }: { text: string; onPress?: () => void }) {
-  return (
-    <Text
-      style={{
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        backgroundColor: "black",
-        color: "white",
-        margin: 20,
-      }}
-      onPress={onPress}
-    >
-      {text}
-    </Text>
   );
 }
--- examples/next-nav/pages/index.tsx
+++ examples/next-nav/pages/index.tsx
@@ -1,2 +1,40 @@
-// use the Home component from our App.tsx file
-export { Home as default } from '../App'
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Link from "next-rn/link";
+import Head from "next-rn/head";
+import Image from "next-rn/image";
+import Script from "next-rn/script";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Head metaInfo={{ title: "my homepage" }} />
+      <Script />
+      <Image
+        style={{
+          width: 50,
+          height: 50,
+        }}
+        source={{
+          uri: "https://reactnative.dev/img/tiny_logo.png",
+        }}
+      />
+      <Text style={styles.text}>Home Screen 🥳</Text>
+      <Link style={{ color: "green", fontSize: 20 }} routeName="profile">
+        Click me to open profile :)
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    margin: 20,
+  },
+});
