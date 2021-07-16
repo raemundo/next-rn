de1014bc0a478138cf70e99b3cd614994a44c0fc
raemundo <amaiplusplus@gmail.com>
2021-07-16 20:29:13 +0200

    refactor: rm unnecessary pkgs

--- src/link/Link/native.tsx
+++ src/link/Link/native.tsx
@@ -1,15 +1,77 @@
-import * as React from 'react'
-import { Text } from 'react-native'
-import NextLink from 'next/link'
-import { LinkMaker, LinkProps } from 'expo-navigation-core'
-import { NextProps, Web } from './types'
+import * as React from "react";
+import { TouchableOpacity, Text } from "react-native";
+import NextLink from "next/link";
+import { LinkProps } from "./LinkProps";
+import { NextProps, Web } from "./types";
+import useRouting from "../../router/use-routing/next";
+import empty from "../../utils/empty";
+import { useCallback } from "react";
+
+// I made this an "HOC" of sorts to let us both use TS generics for each lib and React.forwardRef
+
+/**
+ * Example usage from expo-gatsby-navigation
+ *
+ * ```es6
+ * import React from 'react'
+ * import { LinkMaker, LinkProps } from 'expo-navigation-core'
+ * import { Text } from 'react-native'
+ * import { ExtraLinkProps, GatsbyWebProps } from './types'
+ *
+ * const Link = React.forwardRef(
+ * (
+ * 	props: LinkProps<ExtraLinkProps, GatsbyWebProps>,
+ * 	ref?: React.Ref<Text>
+ * ) => {
+ * 	const Link = LinkMaker<ExtraLinkProps, GatsbyWebProps>()
+ * 	return <Link {...props} ref={ref} />
+ * }
+ * )
+ *
+ * export default React.memo(Link)
+ * ```
+ */
+
+const LinkMaker = <
+  ExtraProps extends object = {},
+  Web extends object = {},
+  Params extends object = {}
+>() =>
+  React.forwardRef(
+    (props: LinkProps<ExtraProps, Web, Params>, ref?: React.Ref<Text>) => {
+      const { navigate } = useRouting();
+      const {
+        touchableOpacityProps = empty.object,
+        routeName,
+        params,
+        children,
+      } = props;
+
+      const nav = useCallback(
+        () =>
+          navigate({
+            routeName: routeName || "/",
+            params,
+          }),
+        [navigate, routeName, params]
+      );
+
+      return (
+        <TouchableOpacity {...touchableOpacityProps} onPress={nav}>
+          <Text style={props.style} accessibiltyRole="link">
+            {children}
+          </Text>
+        </TouchableOpacity>
+      );
+    }
+  );
 
 const Link = React.forwardRef<Text, LinkProps<NextProps, Web>>(function Link(
   props,
   ref
 ) {
-  const L = LinkMaker<NextProps, Web>() // we have to do this to be able to forward refs 🙃
-  return <L {...props} ref={ref} />
-})
+  const L = LinkMaker<NextProps, Web>(); // we have to do this to be able to forward refs 🙃
+  return <L {...props} ref={ref} />;
+});
 
-export default Link
+export default Link;
--- src/router/use-routing/native.ts
+++ src/router/use-routing/native.ts
@@ -1,1 +1,34 @@
-export { useRouting as default } from 'expo-navigation-core'
+import { useNavigation } from 'react-navigation-hooks'
+import { useCallback } from 'react'
+import { NavigateTo } from './types'
+
+export default function useRouting() {
+  const {
+    navigate: nav,
+    getParam: grabParam,
+    push: pushTo,
+    goBack,
+  } = useNavigation()
+
+  const navigate = useCallback(
+    <To extends NavigateTo = NavigateTo>(route: To) => {
+      nav({
+        routeName: route.routeName,
+        params: route.params,
+      })
+    },
+    [nav]
+  )
+  const push = useCallback(
+    <To extends NavigateTo = NavigateTo>(route: To) => {
+      pushTo(route)
+    },
+    [pushTo]
+  )
+  const getParam = <Param>(param: string, fallback?: unknown): Param => {
+    const value: Param = grabParam(param, fallback)
+    return value
+  }
+
+  return { navigate, getParam, push, goBack: () => goBack() }
+}
