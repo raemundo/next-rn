3f836f941f7cf8a55847b2e0fd6de4dbdc05767a
raemundo <eimooamai@outlook.com>
2021-10-10 18:24:25 +0200

    build(deps): add expo-localization

--- examples/with-next/components/LocaleSwitcher.tsx
+++ examples/with-next/components/LocaleSwitcher.tsx
@@ -4,14 +4,17 @@ import { View, Text } from "react-native";
 import useRouting from "next-rn/router/use-routing";
 
 export default function LocaleSwitcher() {
-  // const router_ = useRouter()
+  const router_ = useRouter()
   // const router = useRouting();
 
   // const { locales, locale: activeLocale } = router
   // const otherLocales = locales.filter((locale) => locale !== activeLocale)
   return (
     <View>
-      <Text>Locale switcher:</Text>
+      <Text></Text>
+      <Link routeName="profile" params={{ id: "ahmed" }} web={{ path: "profile/[id]" }} locale={router_.locale === 'en' ? 'de' : 'en'}>  
+        Locale switcher:
+      </Link>
       {/* <ul>
         {otherLocales.map((locale) => {
           const { asPath } = router_
